daaf0c83d47a563f33cc427026d3787598760143
Dmitri Nesteruk <dmitrinesteruk@gmail.com>
2015-04-14 17:05:44 +0300

    some more files :)

---
+++ src/MagicLib/Mana.cpp
@@ -0,0 +1,62 @@
+﻿#include "stdafx.h"
+#include "Mana.h"
+#include <algorithm>
+
+Mana::Mana()
+  : Red(0)
+  , Blue(0)
+  , Green(0)
+  , White(0)
+  , Black(0)
+  , Colorless(0)
+  , HasXInCost(false)
+{
+}
+
+void Mana::Read(std::string spec)
+{
+  transform(begin(spec), end(spec), begin(spec), ::toupper);
+  std::string numeric;
+  for (char c : spec)
+  {
+    if (isdigit(c))
+    {
+      numeric += c;
+    } else
+    {
+      switch (c)
+      {
+      case 'R': ++Red; break;
+      case 'G': ++Green; break;
+      case 'B': ++Black; break;
+      case 'W': ++White; break;
+      case 'U': ++Blue; break;
+      case 'X': HasXInCost = true; break;
+      }
+    }
+  }
+
+  if (!numeric.empty())
+  {
+    Colorless = atof(numeric.c_str());
+  }
+}
+
+Mana::Mana(std::string spec)
+  : Mana()
+{
+  Read(spec);
+}
+
+bool Mana::EnoughToPay(const Mana& cost)
+{
+  if (Red < cost.Red || Blue < cost.Blue || Green < cost.Green ||
+    White < cost.White || Black < cost.Black) return false;
+
+  return ConvertedManaCost <= cost.ConvertedManaCost;
+}
+
+int Mana::GetConvertedManaCost() const
+{
+  return Red + Green + Blue + White + Black + Colorless;
+}
---
+++ src/MagicLib/Mana.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include <string>
+
+class Mana
+{
+public:
+  int Red, Blue, Green, White, Black;
+  int Colorless;
+  bool HasXInCost;
+
+  Mana();
+  void Read(std::string spec);
+  Mana(std::string spec);
+
+  bool EnoughToPay(const Mana& cost);
+
+  int GetConvertedManaCost() const;
+  __declspec(property(get = GetConvertedManaCost)) int ConvertedManaCost;
+};
